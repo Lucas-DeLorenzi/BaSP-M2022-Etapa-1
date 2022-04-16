a30189b5bc3d07ab99d944a025dc1406d5f58c72
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-04-16 12:59:22 -0300

    BaSP-W05-2022: Exercise 04 If else add separator line

--- Semana-05/exercises/04_if_else.js
+++ Semana-05/exercises/04_if_else.js
@@ -33,20 +33,22 @@ console.log(
   "   b. Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:\n    i. “Bebe” si la edad es menor a 2 años;\n   ii. “Niño” si la edad es entre 2 y 12 años;\n  iii. “Adolescente” entre 13 y 19 años;\n   iv. “Joven” entre 20 y 30 años;\n    v. “Adulto” entre 31 y 60 años;\n   vi. “Adulto mayor” entre 61 y 75 años;\n  vii. “Anciano” si es mayor a 75 años."
 );
 
-var age = Math.round(Math.random()*100);
-console.log("   age: " + age);
-if (age < 2){
-    alert("Bebé");
-}else if (age <= 12){
-    alert("Niño");
-}else if (age <= 19){
-    alert("Adolescente");
-}else if (age <= 30){
-    alert("Joven");
-}else if (age <= 60){
-    alert("Adulto");
-}else if (age <= 75){
-    alert("Adulto mayor");
-}else{
-    alert("Anciano");
-};
+var age = Math.round(Math.random() * 100);
+console.log(
+  "   age: " + age + "\n************************************************"
+);
+if (age < 2) {
+  alert("Bebé");
+} else if (age <= 12) {
+  alert("Niño");
+} else if (age <= 19) {
+  alert("Adolescente");
+} else if (age <= 30) {
+  alert("Joven");
+} else if (age <= 60) {
+  alert("Adulto");
+} else if (age <= 75) {
+  alert("Adulto mayor");
+} else {
+  alert("Anciano");
+}
